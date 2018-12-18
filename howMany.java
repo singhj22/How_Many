37f216868b5858831236f35e69aa0bbcbca67c47
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-12-18 12:41:50 -0500

    Add files via upload

---
+++ howMany.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Jugat Singh
+ *How Many
+ */
+public class howMany 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner user= new Scanner(System.in);
+		Random rand= new Random();
+		
+		System.out.println("How many times would you like to roll: ");
+		int times= user.nextInt();
+		
+		int one= 0;
+		int two= 0;
+		int three= 0;
+		int four= 0;
+		int five= 0;
+		int six=0;
+		
+		
+
+	}
+
+}
